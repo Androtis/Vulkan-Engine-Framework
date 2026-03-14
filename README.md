21d17861d0e06ff9c9d0f6c6120113b544c05de4
Androsis <drojasg25@gmail.com>
2026-03-14 16:28:28 -0400

    Init commit

---
+++ README.md
@@ -0,0 +1,43 @@
+# Vulkan Engine Framework
+
+## Abstract / Purpose
+
+*Using the Vulkan Graphics API as a framework for a variety of
+graphical implementations.*
+
+I intend to implement computer graphic related ideas within this framework including The Computer Graphics Triangle, Rotations, OBJ Model Loading, Shadow Mapping, BRDFs, Ray Tracing, PBR Rendering, Screen-Space Effects (**i.e. Bloom**), etc.
+
+I believe that the best way to learn is to implement concepts yourself, I made this with the end goal of improving my understanding of computer graphics, graphics card programming, and to keep myself consistently working on modern graphical techniques.
+
+I choose Vulkan for this framework because the market is trending towards greater support for Vulkan in general purpose graphical programming, superseeding OpenGL. This comes with the promise of greater control over the GPU and I believe that as Vulkan becomes more widespread, the techniques implemented here will have a long-standing impact.
+
+I created this repository to keep track of where this project will go and to organize my work, as well as demonstrate to others my work.
+
+## Building From Source
+
+In order to compile the code from source code you will first need:
+
+- Your choice of C++ compiler set to **C++20**, For Windows I'd recommend [G++](https://www.mingw-w64.org/) meanwhile on MacOS or Linux you can use your own Package Manager.
+- Install [Make](https://www.gnu.org/software/make/).
+- Install [CMake]((https://cmake.org/getting-started/)).
+- Build [VCPKG](https://github.com/microsoft/vcpkg) from source and set the build folder as an enviornment variable under $VCPKG_ROOT.
+
+``` bash
+# Make a safe folder to store the repo
+mkdir VulkanEngineFramework
+cd VulkanEngineFramework/
+
+# Clone the repository
+git clone git@github.com:Androtis/Vulkan-Engine-Framework.git
+cd VulkanEngine
+
+# Build
+cmake build
+cd build/
+make
+
+# Run
+./VulkanEngine
+```
+
+**This README is subject to revision and the above is not finalized.**
---
+++ VulkanEngine/main.cpp
@@ -0,0 +1,119 @@
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+
+#include <vulkan/vulkan.h>
+
+#include <iostream>
+#include <stdexcept>
+#include <cstdlib>
+
+
+// Class for handling the computer graphics triangle
+class HelloTriangleApplication {
+public:
+    //Run
+    void run() {
+        initWindow();
+        initVulkan();
+        mainLoop();
+        cleanup();
+    }
+
+private:
+    //Variables
+    //GLFW Window
+    GLFWwindow* window;
+    const uint32_t HEIGHT = 600;
+    const uint32_t WIDTH = 800;
+
+    //Vulkan
+    VkInstance instance;
+
+    
+
+    //Functions
+    void initWindow() {
+        glfwInit();
+
+        glfwWindowHint(GLFW_CLIENT_API,GLFW_NO_API);
+        glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+
+        window = glfwCreateWindow(WIDTH, HEIGHT, "Vulk-an", nullptr, nullptr);
+    }
+
+    void createInstance() {
+        //Describe application info
+        VkApplicationInfo appInfo{};
+        appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+        appInfo.pApplicationName = "Hello, Triangle";
+        appInfo.applicationVersion =  VK_MAKE_VERSION(1,0,0);
+        appInfo.pEngineName = "No Engine";
+        appInfo.engineVersion = VK_MAKE_VERSION(1,0,0);
+        appInfo.apiVersion = VK_API_VERSION_1_0;
+
+        //Create instance info off of the application info
+        VkInstanceCreateInfo createInfo{};
+        createInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+        createInfo.pApplicationInfo = &appInfo;
+
+        //Get the extension amount
+        uint32_t glfwExtensionCount = 0;
+        const char** glfwExtensions;
+    
+        glfwExtensions = glfwGetRequiredInstanceExtensions(&glfwExtensionCount);
+
+        createInfo.enabledExtensionCount = glfwExtensionCount;
+        createInfo.ppEnabledExtensionNames = glfwExtensions;
+        createInfo.enabledLayerCount = 0;
+
+        //Create the vulkan instance
+        if (vkCreateInstance(&createInfo, nullptr, &instance) != VK_SUCCESS) {
+            throw std::runtime_error("failed to create instance!");
+        }
+
+        //Check all the extensions available
+        uint32_t extensionCount = 0;
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, nullptr);
+
+        std::vector<VkExtensionProperties> extenstions(extensionCount);
+        vkEnumerateInstanceExtensionProperties(nullptr, &extensionCount, extenstions.data());
+        
+        std::cout << "available extensions\n";
+
+        for(const auto& extenstion : extenstions) {
+            std::cout << "\t" << extenstion.extensionName << "\n";
+        }
+
+    }
+
+    void initVulkan() {
+        createInstance();
+    }
+
+    void mainLoop() {
+        while (!glfwWindowShouldClose(window)){
+            glfwPollEvents();
+        }
+    }
+
+    void cleanup() {
+        vkDestroyInstance(instance, nullptr);
+
+        glfwDestroyWindow(window);
+
+        glfwTerminate();
+    }
+};
+
+int main() {
+    HelloTriangleApplication app;
+
+    try {
+        app.run();
+    } catch (const std::exception& e) {
+        std::cerr << e.what() << std::endl;
+        return EXIT_FAILURE;
+    }
+
+    return EXIT_SUCCESS;
+}
